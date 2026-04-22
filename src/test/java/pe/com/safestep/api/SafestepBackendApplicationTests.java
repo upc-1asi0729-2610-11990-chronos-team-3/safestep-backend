38a41affe5e91c247dc3556545af34ac84bc7e5b
Jorge Ayala <jayala42000@gmail.com>
2026-04-22 17:16:31 -0500

    chore: SafeStep Backend Initialization!!

---
+++ src/test/java/pe/com/safestep/api/SafestepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.safestep.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafestepBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
