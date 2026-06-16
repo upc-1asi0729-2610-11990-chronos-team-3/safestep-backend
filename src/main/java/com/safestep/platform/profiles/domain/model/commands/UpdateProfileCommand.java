fb7613c44221f76e79011d5e62585f92aa128a85
angelfdevs <angelfloreseusebio@gmail.com>
2026-06-16 14:33:11 -0500

    feat: add Profiles command models

---
+++ src/main/java/com/safestep/platform/profiles/domain/model/commands/UpdateProfileCommand.java
@@ -0,0 +1,5 @@
+package com.safestep.platform.profiles.domain.model.commands;
+
+public record UpdateProfileCommand(Long profileId, String firstName, String lastName, String email, String street,
+        String number, String city, String postalCode, String country) {
+}
